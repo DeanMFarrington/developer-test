d6edf1bbd987c160ad3efd83f60dbe5521417b98
Dean Farrington <deanfarrington@outlook.com>
2016-12-10 23:41:27 +0000

    [Obj-2] Viewing Commands created.  - Commands set up ready to be used.

---
+++ OrangeBricks.Web/Controllers/Viewings/Commands/AcceptViewingCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using OrangeBricks.Web.Models;
+
+namespace OrangeBricks.Web.Controllers.Viewings.Commands
+{
+    public class AcceptViewingCommandHandler
+    {
+        private readonly IOrangeBricksContext _context;
+
+        public AcceptViewingCommandHandler(IOrangeBricksContext context)
+        {
+            _context = context;
+        }
+
+        public void Handle(AcceptViewingCommand command)
+        {
+            var viewing = _context.Viewings.Find(command.ViewingId);
+
+            viewing.UpdatedAt = DateTime.Now;
+            viewing.Status = ViewingStatus.Accepted;
+
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ OrangeBricks.Web/Controllers/Viewings/Commands/RejectViewingCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using OrangeBricks.Web.Models;
+
+namespace OrangeBricks.Web.Controllers.Viewings.Commands
+{
+    public class RejectViewingCommandHandler
+    {
+        private readonly IOrangeBricksContext _context;
+
+        public RejectViewingCommandHandler(IOrangeBricksContext context)
+        {
+            _context = context;
+        }
+
+        public void Handle(RejectViewingCommand command)
+        {
+            var viewing = _context.Viewings.Find(command.ViewingId);
+
+            viewing.UpdatedAt = DateTime.Now;
+            viewing.Status = ViewingStatus.Rejected;
+
+            _context.SaveChanges();
+        }
+    }
+}
