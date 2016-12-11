4c128961e175e4b71876d61ed9dc836de8ca0619
Dean Farrington <deanfarrington@outlook.com>
2016-12-11 19:06:09 +0000

    [Obj-2] Begun Viewings Views.  - Controller updated and Views added, using commented Offers code for future reference.

--- OrangeBricks.Web/Controllers/Viewings/ViewingsController.cs
+++ OrangeBricks.Web/Controllers/Viewings/ViewingsController.cs
@@ -1,6 +1,8 @@
 ﻿using System.Web.Mvc;
 using Microsoft.AspNet.Identity;
 using OrangeBricks.Web.Attributes;
+using OrangeBricks.Web.Controllers.Viewings.Builders;
+using OrangeBricks.Web.Controllers.Viewings.Commands;
 using OrangeBricks.Web.Models;
 
 namespace OrangeBricks.Web.Controllers.Viewings
@@ -13,5 +15,53 @@ namespace OrangeBricks.Web.Controllers.Viewings
         {
             _context = context;
         }
+
+        #region Seller
+
+        [OrangeBricksAuthorize(Roles = "Seller")]
+        public ActionResult OnProperty(int id)
+        {
+            var builder = new ViewingsOnPropertyViewModelBuilder(_context);
+            var viewModel = builder.Build(id);
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [OrangeBricksAuthorize(Roles = "Seller")]
+        public ActionResult Accept(AcceptViewingCommand command)
+        {
+            var handler = new AcceptViewingCommandHandler(_context);
+
+            handler.Handle(command);
+
+            return RedirectToAction("OnProperty", new { id = command.PropertyId });
+        }
+
+        [HttpPost]
+        [OrangeBricksAuthorize(Roles = "Seller")]
+        public ActionResult Reject(RejectViewingCommand command)
+        {
+            var handler = new RejectViewingCommandHandler(_context);
+
+            handler.Handle(command);
+
+            return RedirectToAction("OnProperty", new { id = command.PropertyId });
+        }
+
+        #endregion
+
+        #region Buyer
+
+        [OrangeBricksAuthorize(Roles = "Buyer")]
+        public ActionResult MyViewings()
+        {
+            var builder = new MyViewingsViewModelBuilder(_context);
+            var viewModel = builder.Build(User.Identity.GetUserId());
+
+            return View(viewModel);
+        }
+
+        #endregion
     }
 }
